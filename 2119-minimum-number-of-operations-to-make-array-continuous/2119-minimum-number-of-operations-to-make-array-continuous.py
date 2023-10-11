2acd012e04c1cd12a73e0f71dc80c02f257bb060
hackingworm <irvine.ye@outlook.com>
2023-10-11 17:49:26 +0800

    Time: 2066 ms (5.19%), Space: 35.7 MB (7.41%) - LeetHub

---
+++ 2119-minimum-number-of-operations-to-make-array-continuous/2119-minimum-number-of-operations-to-make-array-continuous.py
@@ -0,0 +1,33 @@
+class Solution:
+    def getSortedUnique(self, nums: List[int]) -> List[int]:
+        unique = set()
+        for num in nums:
+            if num not in unique:
+                unique.add(num)
+        return sorted(list(unique))
+    
+    def minOperations(self, nums: List[int]) -> int:
+        ops = len(nums) - 1
+        sortedUnique = self.getSortedUnique(nums)
+        print(sortedUnique)
+        for l in range(len(sortedUnique)):
+            b = l
+            r = len(sortedUnique)
+            #print("Start ", b, e)
+            #n = 4
+            while b < r - 1: #and 0 < n:
+                #n -= 1
+                m = (b + r) >> 1
+                if (sortedUnique[m] < sortedUnique[l] + len(nums)):
+                    b = m
+                else:
+                    r = m
+            #print("End ", b, e)
+            #r = e
+            #r = l
+            #while r < len(sortedUnique) and sortedUnique[r] < sortedUnique[l] + len(nums):
+            #    r += 1
+            print(sortedUnique[l], l, r, len(nums) - (r - l))
+            ops = min(len(nums) - (r - l), ops)
+
+        return ops
