25d6d00aa0111fe38d64ee549f32d0aa12f16d12
hackingworm <irvine.ye@outlook.com>
2023-10-23 10:00:20 +0800

    Time: 43 ms (34.73%), Space: 16.3 MB (8.9%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if 0 >= n:
+            if -(2 << 31) == n:
+                return True
+            else:
+                return False
+
+        while 1 < n:
+            m = n
+            n >>= 2
+            print(m, n)
+            if m > n << 2:
+                return False
+
+        return True
