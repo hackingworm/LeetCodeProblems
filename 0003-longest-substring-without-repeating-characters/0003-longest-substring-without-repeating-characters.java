fd44f66627eea71d9fb00aa7300bd88aa951c72a
hackingworm <irvine.ye@outlook.com>
2024-08-11 19:07:29 +0800

    Time: 185 ms (5.17%), Space: 45.4 MB (6.19%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -4,6 +4,11 @@ class Solution {
         for (int i = 0; i < s.length(); i++) {
             int length = 1;
             for (int j = i + 1; j < s.length(); j++) {
+                if (-1 < s.substring(i, j).indexOf(s.charAt(j))) {
+                    break;
+                }
+
+                /*
                 int k;
                 for (k = i; k < j; k++) {
                     if (s.charAt(k) == s.charAt(j))
@@ -13,6 +18,7 @@ class Solution {
                 if (j > k) {
                     break;
                 }
+                */
 
                 length++;
             }
