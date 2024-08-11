3fd10ffb1feee780f5d686cc704fe77c594f7c03
hackingworm <irvine.ye@outlook.com>
2024-08-11 10:47:06 +0800

    Time: 45 ms (30.2%), Space: 44.9 MB (27.55%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,12 +3,6 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (target == nums[i] + nums[j]) {
-                    /*
-                    int[] pair = new int[2];
-                    pair[0] = i;
-                    pair[1] = j;
-                    return pair;
-                    */
                     int[] pair = {i, j};
                     return pair;
                 }
