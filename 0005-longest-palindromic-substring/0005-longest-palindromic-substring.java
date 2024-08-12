9663411400bc1d8950c6a6171e7707fd85fb6681
hackingworm <irvine.ye@outlook.com>
2024-08-13 04:46:51 +0800

    Time: 97 ms (47.57%), Space: 41.9 MB (95.18%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        int start = 0, end = 1, maxLength = 1;
+
+        for (int i = 0; i < s.length() - maxLength; i++) {
+            for (int j = i + maxLength; j < s.length(); j++) {
+                int backward, forward;
+                for (backward = (i + j - 1) >> 1, forward = ((i + j ) >> 1) + 1;
+                    forward <= j; backward--, forward++) {
+                    if (s.charAt(backward) != s.charAt(forward)) {
+                        break;
+                    }
+                }
+
+                // System.out.println("i: " + i + ", j: " + j + ", backward: " + backward + ", forward: " + forward);
+                backward++;
+                if (maxLength < forward - backward) {
+                    maxLength = forward - backward;
+                    start = backward;
+                    end = forward;
+                }
+            }
+        }
+
+        return s.substring(start, end);
+    }
+}
