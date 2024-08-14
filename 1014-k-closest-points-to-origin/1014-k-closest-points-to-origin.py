40cb2f1f334f052fd3f2012d31d7ba2f42dd7f43
hackingworm <irvine.ye@outlook.com>
2024-08-15 01:13:42 +0800

    Time: 584 ms (76.48%), Space: 23.1 MB (36.13%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
@@ -1,13 +1,11 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        eudists = []
-        for point in points:
-            eudists.append([point[0] ** 2 + point[1] ** 2, point])
+        eudists = [[0, [0, 0]]] * len(points)
+        for i in range(len(points)):
+            eudists[i] = [points[i][0] ** 2 + points[i][1] ** 2, points[i]]
         eudists.sort()
-        #print(eudists)
         
         results = [[0, 0]] * k
         for i in range(k):
             results[i] = eudists[i][1]
-
         return results
