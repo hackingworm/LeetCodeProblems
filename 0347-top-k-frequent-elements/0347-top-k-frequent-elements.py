e2a1d86f3ca530c022e8568385590880a8721277
hackingworm <irvine.ye@outlook.com>
2024-08-15 02:13:36 +0800

    Time: 90 ms (51.61%), Space: 20.7 MB (98.33%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -6,9 +6,6 @@ class Solution:
                 freqs[num] = 1
             else:
                 freqs[num] += 1
-        #print(freqs)
         sortedFreqs = dict(sorted(freqs.items(), key = lambda item: item[1], reverse = True))
-        #print(sortedFreqs)
         results = [key for key, value in sortedFreqs.items()]
-        #print(results)
         return results[:k]
