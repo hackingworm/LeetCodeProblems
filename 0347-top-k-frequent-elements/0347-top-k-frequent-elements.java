c85ed7b16fd3f0f80e7a026740ec247a6a5341ba
hackingworm <irvine.ye@outlook.com>
2024-08-24 00:41:45 +0800

    Time: 15 ms (37.08%), Space: 48.6 MB (51.52%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,25 +1,6 @@
 class Solution {
-    public HashMap<Integer, Integer> sortByValue(HashMap<Integer, Integer> hMap) {
-        List<Map.Entry<Integer, Integer>> list
-            = new LinkedList<Map.Entry<Integer, Integer>>(hMap.entrySet());
-
-        Collections.sort(list, new Comparator<Map.Entry<Integer, Integer>>() {
-            public int compare(Map.Entry<Integer, Integer> item1, Map.Entry<Integer, Integer> item2) {
-                return (item2.getValue()) - (item1.getValue());
-
-            }
-        });
-
-        HashMap<Integer, Integer> linked = new LinkedHashMap<Integer, Integer>();
-        for (Map.Entry<Integer, Integer> item: list) {
-            linked.put(item.getKey(), item.getValue());
-        }
-
-        return linked;
-    }
-
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Integer> freqs = new HashMap<Integer, Integer>();
+        Map<Integer, Integer> freqs = new HashMap<Integer, Integer>();
         for (int i: nums) {
             Integer value = freqs.get(i);
             if (null == value) {
@@ -30,13 +11,17 @@ class Solution {
         }
         // System.out.println(freqs);
 
-        Map<Integer, Integer> linked = sortByValue(freqs);
-        // System.out.println(linked);
+        List<Map.Entry<Integer, Integer>> list = new LinkedList<Map.Entry<Integer, Integer>>(freqs.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<Integer, Integer>>() {
+            public int compare(Map.Entry<Integer, Integer> item1, Map.Entry<Integer, Integer> item2) {
+                return item2.getValue() - item1.getValue();
+            }
+        });
 
         int[] results = new int[k];
         int i = 0;
-        for (Integer key: linked.keySet()) {
-            results[i++] = key;
+        for (Map.Entry<Integer, Integer> item: list) {
+            results[i++] = item.getKey();
             if (k == i) {
                 break;
             }
