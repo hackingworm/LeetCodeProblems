f3acca00d18328188878a4de7e0e6bceb0d0f581
hackingworm <irvine.ye@outlook.com>
2024-11-06 00:43:41 +0800

    Time: 2514 ms (5.01%), Space: 19.8 MB (88.47%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,26 +1,59 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        dict = {}
-        for i in range(len(nums)):
-            dict[nums[i]] = i
-        # print(dict)
+        valDict = {}
+        i = 0
+        while len(nums) > i:
+            num = nums[i]
+            if num in valDict:
+                if 0 == num and 3 == valDict[num] or 0 != num and 2 == valDict[num]:
+                    nums.remove(num)
+                    continue
+                else:
+                    valDict[num] += 1
+            else:
+                valDict[num] = 1
+
+            i += 1
+
+        #print(nums, valDict)
 
         triples = set()
-        for i in range(len(nums) - 1):
-            for j in range(i + 1, len(nums)):
+        for i in range(len(nums)):
+            valDict[nums[i]] -= 1
+            for j in range(len(nums)):
+                if 0 == valDict[nums[j]] or nums[i] > nums[j]:
+                    continue
+
+                valDict[nums[j]] -= 1
+                val = -nums[i] - nums[j]
+                if nums[j] <= val and val in valDict and 0 < valDict[val]:
+                    triples.add((nums[i], nums[j], val))
+                valDict[nums[j]] += 1
+            valDict[nums[i]] += 1
+
+        return list(triples)
+
+        '''
+        dict = {}
+        #nums.sort()
+        for i in range(len(nums)):
+            dict[nums[i]] = i
+
+        triples = set()
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if j == i or nums[j] < nums[i]:
+                    continue
+                
                 k = dict.get(-nums[i] - nums[j])
                 if None == k:
                     continue
 
-                if j < k:
-                    #triple = [nums[i], nums[j], nums[k]]
-                    #triple.sort()
-                    #triples.add((triple[0], triple[1], triple[2]))
+                if k != i and k != j and nums[k] >= nums[j]:
                     triples.add((nums[i], nums[j], nums[k]))
 
-        # print(triples)
         return list(triples)
+        '''
 
         '''
         triples = set()
