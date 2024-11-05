ceed20b099b9ad2344289c68d77ae489ec22be91
hackingworm <irvine.ye@outlook.com>
2024-11-06 01:12:46 +0800

    Time: 2135 ms (5.01%), Space: 19.9 MB (75.11%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,31 +1,31 @@
 class Solution:
-    def threeSum(self, origNums: List[int]) -> List[List[int]]:
-        nums = []
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        newNums = []
         valDict = {}
-        for num in origNums:
+        for num in nums:
             if num in valDict:
                 valDict[num] += 1
             else:
                 valDict[num] = 1
 
             if 3 > valDict[num] or 4 > valDict[num] and 0 == num:
-                nums.append(num) 
+                newNums.append(num) 
 
-        #print(nums, valDict)
+        #print(newNums, valDict)
 
         triples = set()
-        for i in range(len(nums)):
-            valDict[nums[i]] -= 1
-            for j in range(len(nums)):
-                if i == j or nums[i] > nums[j]:
+        for i in range(len(newNums)):
+            valDict[newNums[i]] -= 1
+            for j in range(len(newNums)):
+                if i == j or newNums[i] > newNums[j]:
                     continue
 
-                valDict[nums[j]] -= 1
-                val = -nums[i] - nums[j]
-                if nums[j] <= val and val in valDict and 0 < valDict[val]:
-                    triples.add((nums[i], nums[j], val))
-                valDict[nums[j]] += 1
-            valDict[nums[i]] += 1
+                valDict[newNums[j]] -= 1
+                val = -newNums[i] - newNums[j]
+                if newNums[j] <= val and val in valDict and 0 < valDict[val]:
+                    triples.add((newNums[i], newNums[j], val))
+                valDict[newNums[j]] += 1
+            valDict[newNums[i]] += 1
 
         return list(triples)
 
