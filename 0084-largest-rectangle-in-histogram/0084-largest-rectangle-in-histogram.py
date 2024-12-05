af8ef051b7f81818de1d9f1cec3fd1e888a89a9b
hackingworm <irvine.ye@outlook.com>
2024-12-06 00:11:32 +0800

    Time: 218 ms (16.04%), Space: 30.1 MB (60.83%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -7,7 +7,7 @@ class Solution:
                 stack.pop()
             leftLess[i] = stack[-1][0]
             stack.append([i, heights[i]])
-        print(leftLess)
+        #print(leftLess)
 
         rightLess = [len(heights)] * len(heights)
         stack = [[len(heights), -1]]
@@ -16,7 +16,7 @@ class Solution:
                 stack.pop()
             rightLess[i] = stack[-1][0]
             stack.append([i, heights[i]])
-        print(rightLess)
+        #print(rightLess)
 
         largest = 0
         for i in range(len(heights)):
@@ -24,51 +24,3 @@ class Solution:
             if largest < current:
                 largest = current
         return largest
-
-        '''
-        largest = 0
-        i = 0
-        while i < len(heights):
-            j = i - 1
-            while 0 <= j and heights[i] <= heights[j]:
-                j -= 1
-            
-            while len(heights) > i + 1 and heights[i] == heights[i + 1]:
-                i += 1
-
-            k = i + 1
-            while len(heights) > k and heights[i] <= heights[k]:
-                k += 1
-
-            print(heights[i], k - j - 1)
-            area = heights[i] * (k - j - 1)  
-            if largest < area:
-                largest = area
-
-            i += 1
-
-        return largest
-        '''
-
-        '''
-        largest = 0
-        minimium = min(heights)
-
-        i = 0
-        while len(heights) > i:
-            while len(heights) > i and minimium == heights[i]:
-                i += 1
-
-            j = i
-
-            while len(heights) > i and minimium < heights[i]:
-                i += 1
-
-            print(j, i)
-            if (j < i):
-                largest = max(largest, self.largestRectangleArea(heights[j: i]))
-        
-        largest = max(largest, minimium * len(heights))
-
-        return largest
-        '''
