664a64c2502dd337dd304764a55fea97a7f0ec62
hackingworm <irvine.ye@outlook.com>
2024-12-10 03:57:39 +0800

    Time: 60 ms (46.45%), Space: 17.8 MB (12.08%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -8,15 +8,19 @@ class Solution:
         if begin >= end:
             return True
 
+        # Something wrong, so it comes here.
         savedBegin = begin
         savedEnd = end
 
+        # First, try to delete a character in the left side, then continue to check
         begin += 1
         while begin < end and s[begin] == s[end]:
             begin += 1
             end -= 1
         if begin >= end:
             return True
+
+        # If it still fails, then delete a character in the right side this time
 
         begin = savedBegin
         end = savedEnd - 1
