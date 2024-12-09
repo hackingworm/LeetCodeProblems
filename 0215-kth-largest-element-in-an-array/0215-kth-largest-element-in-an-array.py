ffb0880fde1420a2068061079baebfd111d705bb
hackingworm <irvine.ye@outlook.com>
2024-12-09 22:23:35 +0800

    Time: 1199 ms (5.01%), Space: 28.3 MB (33.65%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -12,23 +12,7 @@ class Solution:
                 begin = middle + 1
                 end = middle
 
-        if (8221 == num):
-            print(num, begin, len(sorted))
-            for i in range(begin - 1, begin + 4):
-                print(sorted[i])
         sorted.insert(begin, num)
-
-        '''
-        i = len(sorted)
-        while 0 < i and sorted[i - 1] < num:
-            i -= 1
-        
-        if (8221 == num):
-            print(num, i, len(sorted))
-            for j in range(i - 2, i + 3):
-                print(sorted[j])
-        #sorted.insert(i, num)
-        '''
 
     def findKthLargest(self, nums: List[int], k: int) -> int:
         kLargests = []
