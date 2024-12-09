d29a3c6df6f09b1a5863237f93c3bca7a11a135e
hackingworm <irvine.ye@outlook.com>
2024-12-10 04:34:55 +0800

    Time: 1183 ms (5.02%), Space: 28.3 MB (32.93%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,4 +1,5 @@
 class Solution:
+    # Insert and keep order. Binary search is used to enhance performance 
     def insert(self, num: int, sorted: List[int]):
         begin = 0
         end = len(sorted)
@@ -15,12 +16,17 @@ class Solution:
         sorted.insert(begin, num)
 
     def findKthLargest(self, nums: List[int], k: int) -> int:
+        # This array is used to keep K largest numbers in 'nums' in descent order
         kLargests = []
 
+        # Construct kLargests
         for num in nums:
+            # If there're still not enough "largests" or the current element in 'nums' (num) is larger then the smallest "largests" (the last element in kLargests), then insert 'num' in kLargests, and keep its descent order.
             if k > len(kLargests) or kLargests[-1] < num:
                 if k == len(kLargests):
+                    # If kLargests is "full", pop up the smallest one
                     kLargests.pop()
                 self.insert(num, kLargests)
 
+        # Kth element in kLargest is just what wanted
         return kLargests[-1]
