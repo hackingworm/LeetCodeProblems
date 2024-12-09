f2ceba0ef513e7d45ff822f092ce480fd189ea6c
hackingworm <irvine.ye@outlook.com>
2024-12-10 01:59:08 +0800

    Time: 0 ms (100%), Space: 17.4 MB (6.91%) - LeetHub

---
+++ 0807-custom-sort-string/0807-custom-sort-string.py
@@ -0,0 +1,24 @@
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        freqs = {}
+        for char in order:
+            freqs[char] = 0
+        #print(freqs)
+
+        result = ''
+        for char in s:
+            if char in freqs:
+                freqs[char] += 1
+            else:
+                result += char
+
+        #print(freqs)
+        #print(result)
+
+        for char in order:
+            print(char, freqs[char])
+            for i in range(freqs[char]):
+                result += char
+
+        #print(result)
+        return result
