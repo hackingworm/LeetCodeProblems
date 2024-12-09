4a2588dd489ee0882dad7d56416958a4eba6f8e6
hackingworm <irvine.ye@outlook.com>
2024-12-10 04:19:05 +0800

    Time: 0 ms (100%), Space: 17.2 MB (12.39%) - LeetHub

--- 0807-custom-sort-string/0807-custom-sort-string.py
+++ 0807-custom-sort-string/0807-custom-sort-string.py
@@ -1,24 +1,23 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
+        # Construct a map, the key is the characters in "order", while the value is the character's occurance time in 's'
         freqs = {}
         for char in order:
             freqs[char] = 0
-        #print(freqs)
 
-        result = ''
+        result = '' # Initialize the result string
+        # Check 's'
         for char in s:
+            # If the charater is in 'order', the increase its occurance time
             if char in freqs:
                 freqs[char] += 1
+            # Else put it into the result string directly
             else:
                 result += char
 
-        #print(freqs)
-        #print(result)
-
+        # Put the characters in 'order' per its occurance time in 's'
         for char in order:
-            #print(char, freqs[char])
             for i in range(freqs[char]):
                 result += char
-
-        #print(result)
+                
         return result
