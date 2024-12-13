4c5bd33932c7b5635dd95454a58e74fc35a237ff
hackingworm <irvine.ye@outlook.com>
2024-12-13 09:07:02 +0800

    Time: 10475 ms (5.11%), Space: 20.4 MB (15.91%) - LeetHub

---
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -0,0 +1,53 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def heightOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        if None == root.left and None == root.right:
+            return 0
+            
+        if None == root.left:
+            hLeft = 0
+        else:
+            hLeft = self.heightOfBinaryTree(root.left)
+
+        if None == root.right:
+            hRight = 0
+        else:
+            hRight = self.heightOfBinaryTree(root.right)
+
+        #print(root.val, hLeft, hRight)
+        return max(hLeft, hRight) + 1
+
+        
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        if None == root or None == root.left and None == root.right:
+            return 0
+
+        onlyChild = None
+        if None == root.left:
+            onlyChild = root.right
+
+        if None == root.right:
+            onlyChild = root.left
+
+        if None != onlyChild:
+            childDiameter = self.diameterOfBinaryTree(onlyChild)
+            if self.heightOfBinaryTree(onlyChild) == childDiameter:
+                childDiameter += 1
+            return childDiameter
+
+        diameter = self.heightOfBinaryTree(root.left) + self.heightOfBinaryTree(root.right) + 2
+
+        childDiameter = self.diameterOfBinaryTree(root.left)
+        if childDiameter > diameter:
+            diameter = childDiameter
+        
+        childDiameter = self.diameterOfBinaryTree(root.right)
+        if childDiameter > diameter:
+            diameter = childDiameter
+
+        return diameter
