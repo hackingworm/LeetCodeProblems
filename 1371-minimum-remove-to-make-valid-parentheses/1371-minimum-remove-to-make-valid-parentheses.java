1d20af262ad1e6b3eaf1464e27cfd945ee419c14
hackingworm <irvine.ye@outlook.com>
2024-12-28 21:14:21 +0800

    Time: 30 ms (20.34%), Space: 45.8 MB (16.42%) - LeetHub

---
+++ 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        Stack<Integer> indices = new Stack<>();
+        int index = 0;
+        while (s.length() > index) {
+            char ch = s.charAt(index);
+            if ('(' == ch) {
+                indices.push(index);
+                index++;
+                continue;
+            }
+
+            if (')' == ch) {
+                if (!indices.empty()) {
+                    indices.pop();
+                    index++;
+                    continue;
+                }
+                
+                s = s.substring(0, index) + s.substring(index + 1, s.length());
+                continue;
+            }
+
+            index++;
+        }
+
+        while (!indices.empty()) {
+            index = indices.pop();
+            s = s.substring(0, index) + s.substring(index + 1, s.length());
+        }
+
+        return s;
+    }
+}
