1f1f3718959f5dddf915e7dfd19e6ead33efdaf3
hackingworm <irvine.ye@outlook.com>
2024-12-29 03:14:46 +0800

    Time: 26 ms (33.78%), Space: 46.3 MB (56.19%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -3,32 +3,34 @@ import java.util.regex.Pattern;
 
 class Solution {
     private int muldiv(String s) {
-        Pattern pattern = Pattern.compile("[\\*\\/]");
-        Matcher matcher = pattern.matcher(s);
-
         int product = 1;
-        int last = 0;
+        int from = 0;
         char op = '*';
-        while (matcher.find()) {
-            int start = matcher.start(),
-                end = matcher.end();
+        while (true) {
+            int mul = s.indexOf('*', from),
+                div = s.indexOf('/', from);
 
-            int num = Integer.parseInt(s.substring(last, start).trim());
-            if ('*' == op) {
-                product *= num;
-            } else {
-                product /= num;
+            if (-1 == mul && -1 == div) {
+                break;
             }
 
-            last = end;
-            op = s.charAt(start);
+            int to = -1 == div || -1 != mul && div > mul? mul: div;
+            int value = Integer.parseInt(s.substring(from, to).trim());
+            if ('*' == op) {
+                product *= value;
+            } else {
+                product /= value;
+            }
+
+            from = to + 1;
+            op = s.charAt(to);
         }
 
-        int num = Integer.parseInt(s.substring(last, s.length()).trim());
+        int value = Integer.parseInt(s.substring(from).trim());
         if ('*' ==op) {
-            product *= num;
+            product *= value;
         } else {
-            product /= num;
+            product /= value;
         }
 
         return product;
@@ -47,15 +49,14 @@ class Solution {
             }
 
             int to = -1 == minus || -1 != plus && minus > plus? plus: minus;
-
-            int num = muldiv(s.substring(from, to).trim());
-            sum += '+' == op? num: -num;
+            int value = muldiv(s.substring(from, to).trim());
+            sum += '+' == op? value: -value;
             from = to + 1;
             op = s.charAt(to);
         }
 
-        int num = muldiv(s.substring(from));
-        sum += '+' == op? num: -num;
+        int value = muldiv(s.substring(from).trim());
+        sum += '+' == op? value: -value;
 
         return sum;
     }
