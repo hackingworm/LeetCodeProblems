9e178197f1c6f0fa7951c7508163201359168267
hackingworm <irvine.ye@outlook.com>
2025-01-02 00:04:59 +0800

    Time: 120 ms (5.31%), Space: 47 MB (26.97%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
@@ -2,28 +2,36 @@ class Solution {
     public int longestOnes(int[] nums, int k) {
         int longest = 0;
 
-        for (int start = 0; start < nums.length; start++) {
-            int length = 0;
-            int count = 0;
-            for (int index = start; index < nums.length; index++) {
-                if (0 == nums[index]) {
-                    if (k == count) {
+        int start = 0;
+        while (nums.length > start) {
+            int leadingZeros;
+            for (leadingZeros = 0; nums.length > start && 0 == nums[start]; start++, leadingZeros++);
+
+            int length;
+            for (length = 0; nums.length > start && 1 == nums[start]; length++, start++);
+            int nextStart = start;
+
+            int count = k;
+            for (; nums.length > start; start++) {
+                if (0 == nums[start]) {
+                    if (0 == count) {
                         break;
                     }
 
-                    count++;
+                    count--;
                 }
 
                 length++;
             }
 
+            length += leadingZeros < count? leadingZeros: count;
             if (longest < length) {
                 longest = length;
             }
 
+            start = nextStart;
         }
 
         return longest;
-        
     }
 }
