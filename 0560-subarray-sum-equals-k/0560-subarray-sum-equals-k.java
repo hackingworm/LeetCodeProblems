2b6c804c3c22b18e046488028f69ca4a63afc4df
hackingworm <irvine.ye@outlook.com>
2025-01-02 04:41:00 +0800

    Time: 1578 ms (5.01%), Space: 45.6 MB (98.19%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,17 +1,26 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
         int sums[] = new int[nums.length + 1]; 
-        HashMap<Integer, Integer> sumMap = new HashMap<>();
         sums[0] = 0;
         for (int sum = 0, i = 0; i < nums.length; i++) {
             sum += nums[i];
             sums[i + 1] = sum;
-            if (sumMap.containsKey(sum)) {
-                sumMap.replace(sum, sumMap.get(sum) + 1);
+        }
+
+        /*
+        HashMap<Integer, List<Integer>> sumMap = new HashMap<>();
+        for (int i = 1; i < sums.length; i++) {
+            if (sumMap.containsKey(sums[i])) {
+                List<Integer> list = sumMap.get(sums[i]);
+                list.add(i);
+                Collections.sort(list);
             } else {
-                sumMap.put(sum, 1);
+                List<Integer> list = new ArrayList<>();
+                list.add(i);
+                sumMap.put(sums[i], list);
             }
         }
+        */
 
         int count = 0;
         for (int i = 0; i < nums.length; i++) {
@@ -20,11 +29,16 @@ class Solution {
                     count++;
                 }
             }
+
             /*
             int key = k + sums[i];
             if (sumMap.containsKey(key)) {
-                count += sumMap.get(key);
-            }*/
+                List<Integer> list = sumMap.get(key);
+                int j;
+                for (j = 0; j < list.length() && list[j] <= i ; j++);
+                count += list.length() - j;
+            }
+            */
         }
 
         return count;
