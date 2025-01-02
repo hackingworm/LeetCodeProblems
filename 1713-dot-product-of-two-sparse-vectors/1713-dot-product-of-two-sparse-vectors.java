d94872e59cafdb957c38151f372d104f1313d44b
hackingworm <irvine.ye@outlook.com>
2025-01-03 00:15:23 +0800

    Time: 11 ms (31.54%), Space: 57.8 MB (40.49%) - LeetHub

---
+++ 1713-dot-product-of-two-sparse-vectors/1713-dot-product-of-two-sparse-vectors.java
@@ -0,0 +1,45 @@
+class SparseVector {
+    List<Pair<Integer, Integer>> nonZeros = new ArrayList<>();
+    SparseVector(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (0 != nums[i]) {
+                // Pair<Integer, Integer> pair = new Pair<>(i, nums[i]);
+                nonZeros.add(new Pair<>(i, nums[i]));
+            }
+        }
+        
+    }
+    
+    public List<Pair<Integer, Integer>> getNonZeros() {
+        return nonZeros;
+    }
+
+	// Return the dotProduct of two sparse vectors
+    public int dotProduct(SparseVector vec) {
+        int product = 0;
+        List<Pair<Integer, Integer>> nonZeros2 = vec.getNonZeros();
+        for (int j = 0, i = 0; j < nonZeros2.size() && i < nonZeros.size(); i++) {
+            Pair<Integer, Integer> nonZero = nonZeros.get(i);
+            int index = nonZero.getKey();
+            for (; j < nonZeros2.size(); j++) {
+                Pair<Integer, Integer> nonZero2 = nonZeros2.get(j);
+                int index2 = nonZero2.getKey();
+
+                if (index <= index2) {
+                    if (index == index2) {
+                        product += nonZero.getValue() * nonZero2.getValue();
+                    }
+
+                    break;
+                }
+            }
+        }
+
+        return product;
+    }
+}
+
+// Your SparseVector object will be instantiated and called as such:
+// SparseVector v1 = new SparseVector(nums1);
+// SparseVector v2 = new SparseVector(nums2);
+// int ans = v1.dotProduct(v2);
