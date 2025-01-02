0cb224ba6e386f1b8ab55e6285278de396616677
hackingworm <irvine.ye@outlook.com>
2025-01-02 16:21:27 +0800

    Time: 22 ms (94.9%), Space: 46.7 MB (37.56%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,55 +1,25 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int sums[] = new int[nums.length + 1]; 
-        sums[0] = 0;
+        int count = 0;
+        HashMap<Integer, Integer> sumMap = new HashMap<>();
+        sumMap.put(0, 1);
         for (int sum = 0, i = 0; i < nums.length; i++) {
             sum += nums[i];
-            sums[i + 1] = sum;
-        }
-
-        HashMap<Integer, List<Integer>> sumMap = new HashMap<>();
-        for (int i = 1; i < sums.length; i++) {
-            if (sumMap.containsKey(sums[i])) {
-                List<Integer> list = sumMap.get(sums[i]);
-                list.add(i);
-            } else {
-                List<Integer> list = new ArrayList<>();
-                list.add(i);
-                sumMap.put(sums[i], list);
+            if (sumMap.containsKey(sum - k)) {
+                count += sumMap.get(sum -k);
             }
-        }
 
-        int count = 0;
-        for (int i = 0; i < nums.length; i++) {
+            if (sumMap.containsKey(sum)) {
+                sumMap.replace(sum, sumMap.get(sum) + 1);
+            } else {
+                sumMap.put(sum, 1);
+            }
+            
             /*
-            for (int j = i + 1; j < nums.length + 1; j++) {
-                if (k == sums[j] - sums[i]) {
-                    count++;
-                }
+            if (sumMap.containsKey(sum - k)) {
+                count += sumMap.get(sum - k);
             }
             */
-
-            int key = k + sums[i];
-            if (sumMap.containsKey(key)) {
-                List<Integer> list = sumMap.get(key);
-                int begin = 0,
-                    end = list.size(),
-                    middle = 0;
-                while (begin < end) {
-                    middle = (begin + end) >> 1;
-                    if (i < list.get(middle)) {
-                        end = middle - 1;
-                    } else {
-                        begin = middle + 1;
-                    }
-                }
-
-                if (begin < list.size() && i >= list.get(begin)) {
-                    begin++;
-                }
-
-                count += list.size() - begin;
-            }
         }
 
         return count;
