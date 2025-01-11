502602fc58186c99d41af63ffa7dd4b74da051a5
hackingworm <irvine.ye@outlook.com>
2025-01-11 18:54:24 +0800

    Time: 4 ms (99.71%), Space: 45.6 MB (34.25%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,5 +1,5 @@
 class Solution {
-    boolean validPalindromeImpl(String s, boolean removed) {
+    boolean validPalindrome(String s, boolean removed) {
         int i = 0, j = s.length() - 1;
         for (; i < j; i++, j--) {
             if (s.charAt(i) != s.charAt(j)) {
@@ -8,13 +8,13 @@ class Solution {
                 }
 
                 if (s.charAt(i) == s.charAt(j - 1)) {
-                    if (validPalindromeImpl(s.substring(i, j), true)) {
+                    if (validPalindrome(s.substring(i, j), true)) {
                         return true;
                     }
                 }
                 
                 if (s.charAt(i + 1) == s.charAt(j)) {
-                    if (validPalindromeImpl(s.substring(i + 1, j + 1), true)) {
+                    if (validPalindrome(s.substring(i + 1, j + 1), true)) {
                         return true;
                     }
                 }
@@ -27,6 +27,6 @@ class Solution {
     }
 
     public boolean validPalindrome(String s) {
-        return validPalindromeImpl(s, false);
+        return validPalindrome(s, false);
     }
 }
