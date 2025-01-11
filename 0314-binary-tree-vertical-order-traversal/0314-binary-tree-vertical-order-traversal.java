783d96ea7e17c267e340cccf9bdfebee51f69bcb
hackingworm <irvine.ye@outlook.com>
2025-01-11 13:58:53 +0800

    Time: 2 ms (98.14%), Space: 42.9 MB (58.22%) - LeetHub

--- 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.java
+++ 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.java
@@ -14,31 +14,44 @@
  * }
  */
 class Solution {
+    int numOfOnes(int num) {
+        int count = 0;
+        while (0 < num) {
+            if (1 == num % 2) {
+                count++;
+            }
+
+            num >>= 1;
+        }
+
+        return count;
+    }
+
     public List<List<Integer>> verticalOrder(TreeNode root) {
         List<List<Integer>> results = new ArrayList<>();
         results.add(new ArrayList<Integer>());
 
         TreeNode[] queue = new TreeNode[1];
         queue[0] = root;
-        int[] distr = new int[1];
-        distr[0] = 0;
+        // int[] distr = new int[1];
+        // distr[0] = 0;
         int nonempties = 1;
 
         while (0 < nonempties) {
             TreeNode[] queueNext = new TreeNode[queue.length * 2];
-            int[] distrNext = new int[distr.length * 2];
+            // int[] distrNext = new int[distr.length * 2];
             nonempties = 0;
             
             for (int i = 0; i < queue.length; i++) {
-                distrNext[2 * i] = distr[i];
-                distrNext[2 * i + 1] = distr[i] + 2;
+                // distrNext[2 * i] = distr[i];
+                // distrNext[2 * i + 1] = distr[i] + 2;
 
                 TreeNode node = queue[i];
                 if (null == node) {
                     continue;
                 }
 
-                results.get(distr[i]).add(node.val);
+                results.get(/*distr[i]*/ numOfOnes(i) * 2).add(node.val);
                 if (null != node.left) {
                     queueNext[i * 2] = node.left;
                     nonempties++;
@@ -53,7 +66,7 @@ class Solution {
             results.add(0, new ArrayList<Integer>());
             results.add(results.size(), new ArrayList<Integer>());
             queue = queueNext;
-            distr = distrNext;
+            // distr = distrNext;
         }
         
         for (int i = 0; i < results.size();) {
