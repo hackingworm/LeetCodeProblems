6e648d61239649ed5979404923533b2fc2121f41
hackingworm <irvine.ye@outlook.com>
2025-01-19 10:03:23 +0800

    Time: 276 ms (5.85%), Space: 71.5 MB (92.65%) - LeetHub

--- 0854-making-a-large-island/0854-making-a-large-island.java
+++ 0854-making-a-large-island/0854-making-a-large-island.java
@@ -4,6 +4,7 @@ class Solution {
 
     int[][] nearby(int row, int col) {
         int[][] nearby = new int[4][2];
+
         nearby[0][0] = row;
         nearby[0][1] = col - 1;
         nearby[1][0] = row;
@@ -12,16 +13,19 @@ class Solution {
         nearby[2][1] = col;
         nearby[3][0] = row + 1;
         nearby[3][1] = col;
+
         return nearby;
     }
 
     int expand(int row, int col, int index) {
         grid[row][col] = index;
+
         int size = 1;
-        for (int[] point: nearby(row, col)) {
+        for (int[] point : nearby(row, col)) {
             int r = point[0], c = point[1];
-            if (0 <= r && len > r && 0 <= c && len > c && 1 == grid[r][c])
+            if (0 <= r && len > r && 0 <= c && len > c && 1 == grid[r][c]) {
                 size += expand(r, c, index);
+            }
         }
 
         return size;
@@ -31,35 +35,45 @@ class Solution {
         this.grid = grid;
         len = grid.length;
 
-        List<Integer> isolates = new ArrayList();
+        List<Integer> islands = new ArrayList();
         int index = 2;
-        for (int row = 0; row < len; row++)
-            for (int col = 0; col < len; col++)
-                if (1 == grid[row][col]) 
-                    isolates.add(expand(row, col, index++));
+        for (int row = 0; row < len; row++) {
+            for (int col = 0; col < len; col++) {
+                if (1 == grid[row][col]) {
+                    islands.add(expand(row, col, index++));
+                }
+            }
+        }
 
         int largest = 0;
-        for (int size: isolates)
-            if (largest < size)
+        for (int size : islands) {
+            if (largest < size) {
                 largest = size;
+            }
+        }
 
         for (int row = 0; row < len; row++) {
-            for (int col = 0; col < len; col++)
+            for (int col = 0; col < len; col++) {
                 if (0 == grid[row][col]) {
                     Set<Integer> adjacent = new HashSet();
-                    for (int[] point: nearby(row, col)) {
+                    for (int[] point : nearby(row, col)) {
                         int r = point[0], c = point[1];
-                        if (0 <= r && len > r && 0 <= c && len > c && 1 < grid[r][c])
+                        if (0 <= r && len > r && 0 <= c && len > c
+                                && 1 < grid[r][c]) {
                             adjacent.add(grid[r][c]);
+                        }
                     }
 
                     int size = 1;
-                    for (int idx: adjacent)
-                        size += isolates.get(idx - 2);
+                    for (int idx : adjacent) {
+                        size += islands.get(idx - 2);
+                    }
 
-                    if (largest < size)
+                    if (largest < size) {
                         largest = size;
+                    }
                 }
+            }
         }
 
         return largest;
