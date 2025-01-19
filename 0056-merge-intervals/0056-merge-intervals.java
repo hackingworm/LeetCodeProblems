a3259cc8965883f9e0e2204789ef6188852e4f4a
hackingworm <irvine.ye@outlook.com>
2025-01-20 04:15:08 +0800

    Time: 129 ms (5.56%), Space: 46.2 MB (94.44%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        for (int i = 0; i < intervals.length - 1; i++) {
+            int min = i;
+            for (int j = i; j < intervals.length; j++) {
+                if (intervals[min][0] > intervals[j][0]) {
+                    min = j;
+                }
+            }
+
+            if (min != i) {
+                int[] tmp = intervals[i];
+                intervals[i] = intervals[min];
+                intervals[min] = tmp;
+            }
+        }
+
+        List<int[]> merged = new ArrayList();
+        int[] prev = intervals[0];
+        for (int i = 1; i < intervals.length; i++) {
+            int[] cur = intervals[i];
+            if (prev[1] >= cur[0]) {
+                if (cur[1] > prev[1]) {
+                    prev[1] = cur[1];
+                }
+            } else {
+                merged.add(prev);
+                prev = cur;
+            }
+        }
+        merged.add(prev);
+
+        int[][] result = new int[merged.size()][2];
+        return merged.toArray(result);
+    }
+}
