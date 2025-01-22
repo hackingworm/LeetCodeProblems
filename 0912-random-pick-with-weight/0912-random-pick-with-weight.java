88dc40277c2660a6e9eda23a974ffdbca176afa7
hackingworm <irvine.ye@outlook.com>
2025-01-23 06:01:55 +0800

    Time: 25 ms (52.68%), Space: 49.9 MB (58.72%) - LeetHub

--- 0912-random-pick-with-weight/0912-random-pick-with-weight.java
+++ 0912-random-pick-with-weight/0912-random-pick-with-weight.java
@@ -2,19 +2,58 @@ class Solution {
     int[] accums;
 
     public Solution(int[] w) {
-        accums = w;
+        accums = new int[w.length + 1];
+        accums[0] = 0;
         for (int i = 0; i < accums.length - 1; i++) {
+            accums[i + 1] = w[i];
             accums[i + 1] += accums[i];
+            // System.out.print(accums[i] + " ");
         }
+        // System.out.println(accums[accums.length - 1]);
     }
     
     public int pickIndex() {
         double rnd = Math.random() * accums[accums.length - 1];
+        /*
+        if (3 <= accums.length && accums[1] < rnd && rnd <= accums[2]) {
+            System.out.println(rnd);
+        }
+        */
 
+        /*
         int i;
         for (i = 0; i < accums.length && accums[i] < rnd; i++);
 
         return i;
+        */
+
+        int begin, end;
+        for (begin = 0, end = accums.length - 1; begin < end;) {
+            int middle = (begin + end + 1) / 2;
+            /*
+            if (3 <= accums.length && accums[1] < rnd && rnd <= accums[2]) {
+                System.out.println(begin + " " + end + " " + accums[middle]);
+            }
+            */
+
+            if (accums[middle - 1] < rnd && rnd <= accums[middle]) {
+                return middle - 1;
+            }
+
+            if (rnd <= accums[middle - 1]) {
+                end = middle - 1;
+            } else {
+                begin = middle;
+            }
+        }
+
+        /*
+        if (3 <= accums.length && accums[1] < rnd && rnd <= accums[2]) {
+            System.out.println(begin);
+        }
+        */
+
+        return 0;
     }
 }
 
