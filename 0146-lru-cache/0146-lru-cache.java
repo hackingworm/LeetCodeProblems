090a5829fac6fccb5cc187f75447a0755dfef2d9
hackingworm <irvine.ye@outlook.com>
2025-01-24 01:04:50 +0800

    Time: 42 ms (93.11%), Space: 112.8 MB (80.72%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,69 +1,92 @@
 class LRUCache {
-    class DLLNode {
-        DLLNode prev, next;
-        int key, val;
+    class CacheUnit {
+        CacheUnit prev, next;
+        int key, value;
 
-        DLLNode(int key, int value) {
+        CacheUnit(int key, int value) {
             this.key = key;
-            this.val = value;
+            this.value = value;
         }
     }
 
-    DLLNode head; 
-    DLLNode tail;
-    HashMap<Integer, DLLNode> mpp;
+    CacheUnit head, tail;
+    HashMap<Integer, CacheUnit> map;
     int capacity;
+    int size;
 
-    public LRUCache(int capacity) { //just used to instiantiate the values
-        head = new DLLNode(-1,-1);
-        tail = new DLLNode(-1,-1); 
+    public LRUCache(int capacity) {
+        head = new CacheUnit(0, 0);
+        tail = new CacheUnit(0, 0);
         head.next = tail;
         tail.prev = head;
 
-        mpp = new HashMap<>();
+        map = new HashMap();
 
         this.capacity = capacity;
+        size = 0;
     }
     
+    void detach(CacheUnit cacheUnit) {
+        cacheUnit.prev.next = cacheUnit.next;
+        cacheUnit.next.prev = cacheUnit.prev;
+    }
+
+    void prepend(CacheUnit cacheUnit) {
+        head.next.prev = cacheUnit;
+        cacheUnit.next = head.next;
+        head.next = cacheUnit;
+        cacheUnit.prev = head;
+    }
+
     public int get(int key) {
-        if(!mpp.containsKey(key)) return -1;
-        DLLNode node = mpp.get(key);
-        deleteNode(node);
-        insertAfterHead(node);
-        return node.val;
+        if (!map.containsKey(key)) {
+            return -1;
+        }
+
+        CacheUnit cacheUnit = map.get(key);
+        if (head.next != cacheUnit) {
+            detach(cacheUnit);
+            prepend(cacheUnit);
+        }
+
+        return cacheUnit.value;
     }
     
-    public void put(int key, int value) { 
-        if(mpp.containsKey(key)) {
-            DLLNode node = mpp.get(key);
-            node.val = value;
-            deleteNode(node);
-            insertAfterHead(node);
-            mpp.put(key,node);
-        }
-        else{
-            if(mpp.size() == capacity) {
-                //DLLNode node = mpp.get(tail.prev.val);
-                mpp.remove(tail.prev.key);
-                deleteNode(tail.prev); 
+    public void put(int key, int value) {
+        CacheUnit cacheUnit;
+        if (map.containsKey(key)) {
+            cacheUnit = map.get(key);
+            cacheUnit.value = value;
+            
+            if (head.next != cacheUnit) {
+                detach(cacheUnit);
+                prepend(cacheUnit);
             }
-            DLLNode newNode = new DLLNode(key,value);
-            insertAfterHead(newNode);
-            mpp.put(key,newNode);
+        } else {
+            if (capacity > size) {
+                cacheUnit = new CacheUnit(key, value);
+                prepend(cacheUnit);
+                size++;
+            } else {
+                cacheUnit = tail.prev;
+                map.remove(cacheUnit.key);
+                cacheUnit.key = key;
+                cacheUnit.value = value;
+
+                if (head.next != cacheUnit) {
+                    detach(cacheUnit);
+                    prepend(cacheUnit);
+                }
+            }
+            
+            map.put(key, cacheUnit);
         }
-    }
-
-    public void deleteNode(DLLNode node) {
-        DLLNode prevNode = node.prev;
-        prevNode.next = node.next;
-        node.next.prev = prevNode;
-    }
-
-    public void insertAfterHead(DLLNode node) { 
-        DLLNode afterNode = head.next;
-        head.next = node;
-        node.prev = head;
-        afterNode.prev = node;
-        node.next = afterNode;
     }
 }
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
