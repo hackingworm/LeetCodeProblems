11941fdf6d6ebd883f8c11ecd8a91742c44531a2
hackingworm <irvine.ye@outlook.com>
2025-01-24 20:37:37 +0800

    Time: 3 ms (71.38%), Space: 44.3 MB (76.99%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -12,7 +12,7 @@ class Solution {
     class Heap {
         ListNode[] lists;
         int size;
-        
+
         int compare(int index1, int index2) {
             return lists[index2].val - lists[index1].val;
         }
@@ -20,9 +20,14 @@ class Solution {
         void relocate(int index) {
             while (size / 2 > index) {
                 int target = index * 2 + 1;
-                if (size > target + 1 && (0 < compare(target + 1, target))) target++;
-                if (0 <= compare(index, target))
+                if (size > target + 1 && (0 < compare(target + 1, target))) {
+                    target++;
+                }
+
+                if (0 <= compare(index, target)) {
                     break;
+                }
+
                 ListNode tmp = lists[index];
                 lists[index] = lists[target];
                 lists[target] = tmp;
@@ -31,13 +36,20 @@ class Solution {
         }
 
         ListNode getTop() {
-            if (0 == size) return null;        
+            if (0 == size) {
+                return null;
+            }
+        
             ListNode top = lists[0];
             if (null == lists[0].next) {
                 lists[0] = lists[size - 1];
                 size--;
-            } else lists[0] = lists[0].next;
+            } else {
+                lists[0] = lists[0].next;
+            }
+
             relocate(0);
+
             top.next = null;
             return top;
         }
@@ -45,22 +57,32 @@ class Solution {
         Heap(ListNode[] lists) {
             this.lists = new ListNode[lists.length];
             size = 0;
-            for (int i = 0; i < lists.length; i++)
-                if (null != lists[i]) this.lists[size++] = lists[i];
+            for (int i = 0; i < lists.length; i++) {
+                if (null != lists[i]) {
+                    this.lists[size++] = lists[i];
+                }
+            }
 
-            for (int i = size / 2 - 1; i >= 0; i--) relocate(i);
+            for (int i = size / 2 - 1; i >= 0; i--) {
+                relocate(i);
+            }
         }
     }
 
     public ListNode mergeKLists(ListNode[] lists) {
-        if (0 == lists.length) return null;
+        if (0 == lists.length) {
+            return null;
+        }
+
         ListNode head = new ListNode(0);
         ListNode current = head;
+
         Heap heap = new Heap(lists);
         for (ListNode node = heap.getTop(); null != node; node = heap.getTop()) {
             current.next = node;
             current = node;
         }
+
         return head.next;
     }
 }
