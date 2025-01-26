065a5679390f63529072279939f3bdf7a0d08b70
hackingworm <irvine.ye@outlook.com>
2025-01-26 21:07:24 +0800

    Time: 2 ms (98.91%), Space: 45.1 MB (44.98%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,14 +1,17 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        HashMap<Integer, Integer> map = new HashMap();
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (target == nums[i] + nums[j]) {
-                    int[] pair = {i, j};
-                    return pair;
-                }
+            if (map.containsKey(nums[i])) {
+                result[0] = map.get(nums[i]);
+                result[1] = i;
+                break;
             }
+
+            map.put(target - nums[i], i);
         }
 
-        return null;
+        return result;
     }
 }
