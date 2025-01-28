2ac4a2dce456190addbf28ae654de0907c6bc674
hackingworm <irvine.ye@outlook.com>
2025-01-29 01:16:50 +0800

    Time: 17 ms (13.68%), Space: 48.6 MB (66.98%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,31 +1,45 @@
 class Solution {
+    class KV<K, V extends Comparable<V>> implements Comparable<KV<K, V>> {
+        K key;
+        V value;
+
+        KV(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public int compareTo(KV<K, V> kv) {
+            return value.compareTo(kv.value);
+        }
+    }
+
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> freqs = new HashMap<Integer, Integer>();
-        for (int i: nums) {
-            Integer value = freqs.get(i);
-            if (null == value) {
-                freqs.put(i, 1);
+        HashMap<Integer, Integer> map = new HashMap();
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
             } else {
-                freqs.put(i, value + 1);
+                map.put(num, 1);
             }
         }
 
-        List<Map.Entry<Integer, Integer>> list = new LinkedList<Map.Entry<Integer, Integer>>(freqs.entrySet());
-        Collections.sort(list, new Comparator<Map.Entry<Integer, Integer>>() {
-            public int compare(Map.Entry<Integer, Integer> item1, Map.Entry<Integer, Integer> item2) {
-                return item2.getValue() - item1.getValue();
-            }
-        });
-
-        int[] results = new int[k];
+        KV[] frequences = new KV[map.size()];
         int i = 0;
-        for (Map.Entry<Integer, Integer> item: list) {
-            results[i++] = item.getKey();
-            if (k == i) {
-                break;
-            }
+        for (Map.Entry<Integer, Integer> e : map.entrySet()) {
+            frequences[i] = new KV(e.getKey(), e.getValue());
+            i++;
+        }   
+
+        Arrays.sort(frequences);
+
+        int[] result = new int[k];
+        for (i = 0; i < k; i++) {
+            KV<Integer, Integer> kv = frequences[frequences.length - i - 1];
+            // System.out.println(kv.key + " " + kv.value);
+            result[i] = kv.key;
         }
 
-        return results;
+        return result;
     }
 }
