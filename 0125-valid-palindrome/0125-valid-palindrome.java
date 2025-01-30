5a029e861936d77a8f306cd5757e8c2137804d6f
hackingworm <irvine.ye@outlook.com>
2025-01-31 01:40:39 +0800

    Time: 14 ms (33.87%), Space: 44.9 MB (40.53%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean isPalindrome(String s) {
         s = s.replaceAll("[^0-9A-Za-z]", "").toLowerCase();
-        // System.out.println(s);
         for (int begin = 0, end = s.length() - 1; begin < end; begin++, end--) {
             if (s.charAt(begin) != s.charAt(end)) {
                 return false;
