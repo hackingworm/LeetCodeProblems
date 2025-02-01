6c5992b272ad725f8b21284b37b6a94096d591b5
hackingworm <irvine.ye@outlook.com>
2025-02-02 03:35:25 +0800

    Time: 316 ms (22.82%), Space: 52.4 MB (28.44%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,39 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> triples = new ArrayList();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (0 < i && nums[i - 1] == nums[i]) {
+                continue;
+            }
+
+            HashMap<Integer, int[]> map = new HashMap();
+            int j;
+            for (j = i + 1; j < nums.length && !map.containsKey(nums[j]); j++) {
+                if (i + 1 < j && nums[j - 1] == nums[j]) {
+                    continue;
+                }
+
+                map.put(-(nums[i] + nums[j]), new int[]{nums[i], nums[j]});
+            }
+
+            int from = j;
+            for (; j < nums.length; j++) {
+                if (from < j && nums[j - 1] == nums[j]) {
+                    continue;
+                }
+                
+                if (map.containsKey(nums[j])) {
+                    int[] pairs = map.get(nums[j]);
+                    List<Integer> triple = new ArrayList();
+                    triple.add(pairs[0]);
+                    triple.add(pairs[1]);
+                    triple.add(nums[j]);
+                    triples.add(triple);
+                }
+            }
+        }
+
+        return triples;
+    }
+}
