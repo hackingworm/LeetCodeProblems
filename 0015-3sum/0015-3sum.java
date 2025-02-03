a24c08f4a590a81cb46f5dcb0359b552aacbd95a
hackingworm <irvine.ye@outlook.com>
2025-02-03 11:11:17 +0800

    Time: 312 ms (23%), Space: 52.2 MB (33.78%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -2,6 +2,7 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> triples = new ArrayList();
         Arrays.sort(nums);
+        /*
         boolean[] choosen = new boolean[nums.length];
         for (int i = 0; i < nums.length - 2; i++) {
             if (choosen[i]) {
@@ -39,6 +40,37 @@ class Solution {
                     triple.add(pairs[0]);
                     triple.add(pairs[1]);
                     triple.add(nums[j]);
+                    triples.add(triple);
+                }
+            }
+        }
+        */
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (0 < i && nums[i - 1] == nums[i]) {
+                continue;
+            }
+
+            HashMap<Integer, Integer> map = new HashMap();
+            int j;
+            for (j = i + 1; j < nums.length - 1 && !map.containsKey(nums[j]); j++) {
+                if (i + 1 < j && nums[j - 1] == nums[j]) {
+                    continue;
+                }
+
+                map.put(-nums[i] - nums[j], nums[j]);
+            }
+
+            for (int k = j; k < nums.length; k++) {
+                if (j < k && nums[k - 1] == nums[k]) {
+                    continue;
+                }
+
+                if (map.containsKey(nums[k])) {
+                    List<Integer> triple = new ArrayList();
+                    triple.add(nums[i]);
+                    triple.add(map.get(nums[k]));
+                    triple.add(nums[k]);
                     triples.add(triple);
                 }
             }
