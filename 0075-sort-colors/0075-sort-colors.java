114b6469c7439d1edcc97f8581c3bb9c8a9e1b0d
hackingworm <irvine.ye@outlook.com>
2025-02-05 11:02:15 +0800

    Time: 0 ms (100%), Space: 42.2 MB (17.62%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,29 +1,42 @@
 class Solution {
     public void sortColors(int[] nums) {
+        /*
         for (int i = 0, k = 0, j = nums.length - 1; k <= j;) {
             if (0 == nums[k]) {
-                if (0 != nums[i]) {
-                    nums[k] = nums[i];
-                    nums[i] = 0;
-                } else {
-                    k++;
-                }
-
+                nums[k] = nums[i];
+                nums[i] = 0;
+                k++;
                 i++;
                 continue;
             }
 
             if (2 == nums[k]) {
-                if (2 != nums[j]) {
-                    nums[k] = nums[j];
-                    nums[j] = 2;
-                }
-
+                nums[k] = nums[j];
+                nums[j] = 2;
                 j--;
                 continue;
             }
 
             k++;
+        }
+        */
+
+        int[] counters = new int[3];
+        for (int i = 0; i < nums.length; i++) {
+            counters[nums[i]]++;
+        }
+
+        int i = 0;
+        while (i < counters[0]) {
+            nums[i++] = 0;
+        }
+
+        while (i < counters[0] + counters[1]) {
+            nums[i++] = 1;
+        }
+
+        while (i < nums.length) {
+            nums[i++] = 2;
         }
     }
 }
