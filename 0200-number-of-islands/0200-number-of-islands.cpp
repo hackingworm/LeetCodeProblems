711adecc3092111a840c8eae5e4fdb85cb9ab6cf
hackingworm <irvine.ye@outlook.com>
2025-02-10 10:38:58 +0800

    Time: 17 ms (98.15%), Space: 23.2 MB (32.52%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,38 +1,27 @@
 class Solution {
-    void enQueue(int i, int j, vector<vector<char>>& grid, queue<pair<int, int>>& q) {
-        grid[i][j] = 'i';
-        pair p(i, j);
-        q.push(p);
-    }
-
     void claimIslands(int i, int j, vector<vector<char>>& grid) {
         queue<pair<int, int>> q;
-        // pair<int, int> p(i, j);
         q.push({i, j});
         while (!q.empty()) {
             pair p = q.front();
             q.pop();
 
             if (0 < p.first && '1' == grid[p.first - 1][p.second]) {
-                // enQueue(p.first - 1, p.second, grid, q);
                 grid[p.first - 1][p.second] = 'i';
                 q.push({p.first - 1, p.second});
             }
 
             if (grid.size() - 1 > p.first && '1' == grid[p.first + 1][p.second]) {
-                // enQueue(p.first + 1, p.second, grid, q);
                 grid[p.first + 1][p.second] = 'i';
                 q.push({p.first + 1, p.second});
             }
 
             if (0 < p.second && '1' == grid[p.first][p.second - 1]) {
-                // enQueue(p.first, p.second - 1, grid, q);
                 grid[p.first][p.second - 1] = 'i';
                 q.push({p.first, p.second - 1});
             }
 
             if (grid[p.first].size() - 1 > p.second && '1' == grid[p.first][p.second + 1]) {
-                // enQueue(p.first, p.second + 1, grid, q);
                 grid[p.first][p.second + 1] = 'i';
                 q.push({p.first, p.second + 1});
             }            
