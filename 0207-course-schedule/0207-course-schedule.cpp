ea3602e67b604eaa551f14dd3c7febc3c40b0fd5
hackingworm <irvine.ye@outlook.com>
2025-02-10 20:14:16 +0800

    Time: 3 ms (82.44%), Space: 20.1 MB (35.47%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,13 +1,32 @@
 class Solution {
+    class Course {
+    public:
+        int status;
+        vector<Course*> prereqs;
+
+        Course() {
+            status = 0;
+            prereqs = vector<Course*>();
+        }
+    };
+
+    bool canFinish(Course *course) {
+        course->status = 1;
+        for (auto prereq: course->prereqs) {
+            if (   0 == prereq->status && !canFinish(prereq)
+                || 1 == prereq->status) {
+                return false;
+            }
+        }
+
+        course->status = 2;
+        return true;
+    }
+
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        /*
         sort(prerequisites.begin(), prerequisites.end());
-        /* 
-        for (auto prerequisite: prerequisites) {
-            cout << '[' << prerequisite[0] << ',' << prerequisite[1] << "] ";
-        }
-        cout << endl;
-        */
 
         bool erased = true;
         while (erased) {
@@ -36,5 +55,20 @@ public:
         }
 
         return 0 == prerequisites.size();
+        */
+
+        Course *courses = new Course[numCourses];
+
+        for (auto prerequisite: prerequisites) {
+            courses[prerequisite[0]].prereqs.push_back(&courses[prerequisite[1]]);
+        }
+
+        for (int i = 0; numCourses > i; i++) {
+            if (0 == courses[i].status && !canFinish(&courses[i])) {
+                return false;
+            }
+        }
+
+        return true;
     }
 };
