9ac4536f9fb47261947dcd00a8baaa1d8839efb3
hackingworm <irvine.ye@outlook.com>
2025-02-10 10:25:47 +0800

    Time: 30 ms (39.02%), Space: 23.1 MB (35.98%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -6,13 +6,17 @@ class Solution {
     }
 
     void claimIslands(int i, int j, vector<vector<char>>& grid) {
-        queue<pair<int, int>> q;
-        pair<int, int> p(i, j);
-        q.push(p);
+        // queue<pair<int, int>> q;
+        queue<array<int, 2>> q;
+        // pair<int, int> p(i, j);
+        // q.push(p);
+        q.push({i, j});
         while (!q.empty()) {
-            pair p = q.front();
+            // pair p = q.front();
+            array<int, 2> p = q.front();
             q.pop();
 
+            /*
             if (0 < p.first && '1' == grid[p.first - 1][p.second]) {
                 enQueue(p.first - 1, p.second, grid, q);
             }
@@ -27,7 +31,32 @@ class Solution {
 
             if (grid[p.first].size() - 1 > p.second && '1' == grid[p.first][p.second + 1]) {
                 enQueue(p.first, p.second + 1, grid, q);
-            }            
+            }
+            */
+
+            if (0 < p[0] && '1' == grid[p[0] - 1][p[1]]) {
+                // enQueue(p[0] - 1, p[1], grid, q);
+                grid[p[0] - 1][p[1]] = 'i';
+                q.push({p[0] - 1, p[1]});
+            }
+
+            if (grid.size() - 1 > p[0] && '1' == grid[p[0] + 1][p[1]]) {
+                // enQueue(p[0] + 1, p[1], grid, q);
+                grid[p[0] + 1][p[1]] = 'i';
+                q.push({p[0] + 1, p[1]});
+            }
+
+            if (0 < p[1] && '1' == grid[p[0]][p[1] - 1]) {
+                // enQueue(p[0], p[1] - 1, grid, q);
+                grid[p[0]][p[1] - 1] = 'i';
+                q.push({p[0], p[1] - 1});
+            }
+
+            if (grid[p[0]].size() - 1 > p[1] && '1' == grid[p[0]][p[1] + 1]) {
+                // enQueue(p[0], p[1] + 1, grid, q);
+                grid[p[0]][p[1] + 1] = 'i';
+                q.push({p[0], p[1] + 1});
+            }
         }
     }
 
