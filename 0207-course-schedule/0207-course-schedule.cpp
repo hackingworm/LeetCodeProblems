3c71b4b3e7a34868e014ebd4483db53243cdc006
hackingworm <irvine.ye@outlook.com>
2025-02-10 20:16:30 +0800

    Time: 3 ms (82.44%), Space: 20.1 MB (44.34%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -25,38 +25,6 @@ class Solution {
 
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        /*
-        sort(prerequisites.begin(), prerequisites.end());
-
-        bool erased = true;
-        while (erased) {
-            erased = false;
-            for (int i = 0; prerequisites.size() > i; i++) {
-                int required = prerequisites[i][1];
-                int j = 0, k = prerequisites.size();
-                while (j < k) {
-                    int m = (j + k) / 2;
-                    if (required == prerequisites[m][0]) {
-                        break;
-                    }
-
-                    if (required < prerequisites[m][0]) {
-                        k = m;
-                    } else {
-                        j = m + 1;
-                    }
-                }
-
-                if (j >= k) {
-                    prerequisites.erase(prerequisites.begin() + i);
-                    erased = true;
-                }
-            }
-        }
-
-        return 0 == prerequisites.size();
-        */
-
         Course *courses = new Course[numCourses];
 
         for (auto prerequisite: prerequisites) {
