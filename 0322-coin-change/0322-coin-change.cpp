74a55e41dffecbcd3b6515da2c070dba714170d5
hackingworm <irvine.ye@outlook.com>
2025-02-21 04:03:02 +0800

    Time: 35 ms (50.8%), Space: 24.1 MB (42.91%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,73 @@
+class Solution {
+    // vector<int> coins;
+
+public:
+    /*
+    int coinChange(int length, int amount) {
+        int last = coins[length - 1];
+        int quo = amount / last;
+        int remain = amount % last;
+
+        if (0 == remain) {
+            return quo;
+        }
+
+        if (1 == length) {
+            return -1;
+        }
+
+        int total = -1;
+        for (;;) {
+            int sub = coinChange(length - 1, remain);
+            if (-1 != sub && (-1 == total || total > sub + quo)) {
+                    total = quo + sub;
+            }
+
+            if (0 == quo) {
+                break;
+            }
+
+            remain += last;
+            quo--;
+        }
+
+        return total;
+    }
+    */
+
+    int coinChange(vector<int>& coins, int amount) {
+        if (0 == amount) {
+            return 0;
+        }
+
+        sort(coins.begin(), coins.end());
+        // this->coins = coins;
+        // return coinChange(coins.size(), amount);
+        vector<int> results;
+        for (int i = 0; i < amount; i++) {
+            int rst = -1;
+            for (int j = 0; j < coins.size(); j++) {
+                int tmp = i + 1 - coins[j];
+                if (0 > tmp) {
+                    tmp = -1;
+                } else if (0 == tmp) {
+                    tmp = 1;
+                } else {
+                    if (-1 == results[tmp - 1]) {
+                        tmp = -1;
+                    } else {
+                        tmp = results[tmp - 1] + 1;
+                    }
+                }
+
+                if (-1 != tmp && (-1 == rst || rst > tmp)) {
+                    rst = tmp;
+                }
+            }
+
+            results.push_back(rst);
+        }
+
+        return results[results.size() - 1];
+    }
+};
