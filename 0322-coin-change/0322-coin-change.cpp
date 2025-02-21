b74a294d1b4e30bf7eeac4d58cb923317714c4a3
hackingworm <irvine.ye@outlook.com>
2025-02-21 21:20:38 +0800

    Time: 42 ms (41.67%), Space: 19.2 MB (50.49%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -2,56 +2,25 @@
 
 class Solution {
     #if defined(DP)
-    vector<int> coins;
+    // vector<int> coins;
     int* results;
 
-    /*
-    int coinChange(int length, int amount) {
-        int last = coins[length - 1];
-        int quo = amount / last;
-        int remain = amount % last;
-
-        if (0 == remain) {
-            return quo;
-        }
-
-        if (1 == length) {
-            return -1;
-        }
-
-        int total = -1;
-        for (;;) {
-            int sub = coinChange(length - 1, remain);
-            if (-1 != sub && (-1 == total || total > sub + quo)) {
-                    total = quo + sub;
-            }
-
-            if (0 == quo) {
-                break;
-            }
-
-            remain += last;
-            quo--;
-        }
-
-        return total;
-    }
-    */
-
-    int coinChange(int amount) {
+    void /*int*/ coinChangeInner(vector<int>& coins, int amount) {
         if (0 != results[amount- 1]) {
-            return results[amount - 1];
+            return /* results[amount - 1] */;
         }
 
         int least = -1;
         for (auto coin: coins) {
             int rst = amount - coin;
             if (0 == rst) {
-                return results[amount - 1] = 1;
+                results[amount - 1] = 1;
+                return;
             }
 
             if (0 < rst) {
-                rst = coinChange(rst);
+                coinChangeInner(coins, rst);
+                rst = results[rst - 1];
                 if (0 < rst) {
                     rst++;
                     if (-1 == least || least > rst) {
@@ -61,7 +30,7 @@ class Solution {
             }
         }
 
-        return results[amount - 1] = least;
+        /* return*/ results[amount - 1] = least;
     }
     #endif
 
@@ -74,14 +43,14 @@ public:
         sort(coins.begin(), coins.end());
 
         #if defined(DP)
-        this->coins = coins;
+        // this->coins = coins;
 
         results = new int[amount];
         for (int i = 0; i < amount; i++) {
             results[i] = 0;
         }
 
-        coinChange(amount);
+        coinChangeInner(coins, amount);
         #else
 
         int* results = new int[amount];
