742d64ef81fe08031407fd4ec753f807168c9dc1
hackingworm <irvine.ye@outlook.com>
2025-03-12 20:50:37 +0800

    Time: 0 ms (100%), Space: 11 MB (57.24%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -2,19 +2,45 @@ class Solution {
 public:
     int numberOfSubstrings(string s) {
         int num = 0;
-        for (int i = 0; i < s.length() - 2; i++) {
-            bool a = false, b = false, c = false;
-            int j;
-            for (j = i; j < s.length() && (!a || !b || !c); j++) {
-                a |= 'a' == s[j];
-                b |= 'b' == s[j];
-                c |= 'c' == s[j];
+        int begin = 0, last = 0;
+        int ai = -1, bi = -1, ci = -1;
+
+        for (;;) {
+            int sub = 0;
+            for (int i = last; i < s.length(); i++) {
+                if ('a' == s[i]) {
+                    ai = i;
+                } else if ('b' == s[i]) {
+                    bi = i;
+                } else if ('c' == s[i]) {
+                    ci = i;
+                }
+
+                if (!(-1 == ai || -1 == bi || -1 == ci)) {
+                    int first = min(min(ai, bi), ci);
+                    last = max(max(ai, bi), ci);
+                    sub = (first - begin + 1) * (s.length() - last);
+
+                    if (ai == first) {
+                        ai = -1;
+                    } else if (bi == first) {
+                        bi = -1;
+                    } else {
+                        ci = -1;
+                    }
+
+                    begin = first + 1;
+                    last++;
+                    break;
+                }
             }
 
-            if (a && b && c) {
-                num += s.length() - j + 1;
+            if (0 == sub) {
+                break;
             }
-        }
+
+            num += sub;
+        }   
         
         return num;
     }
