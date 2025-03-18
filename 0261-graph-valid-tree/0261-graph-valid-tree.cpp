9ac57f134319b46e4af7989990b0c771761b3fff
hackingworm <irvine.ye@outlook.com>
2025-03-18 12:44:42 +0800

    Time: 7 ms (24.52%), Space: 16.2 MB (84.4%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.cpp
+++ 0261-graph-valid-tree/0261-graph-valid-tree.cpp
@@ -1,53 +1,44 @@
 class Solution {
 public:
     bool validTree(int n, vector<vector<int>>& edges) {
-        /*
         for (int i = 0; i < edges.size(); i++) {
-            int from = edges[i][0], to = edges[i][1];
-            if (from > to) {
-                edges[i][0] = to;
-                edges[i][1] = -from;
-            } else {
-                edges[i][1] = -to;
+            int first = edges[i][0], second = edges[i][1];
+            if (first > second) {
+                edges[i][0] = second;
+                edges[i][1] = first;
             }
         }
-        */
 
         vector<int> counters(n, 0);
         counters[0] = 1;
         int resolved = 1;
-
-        vector<vector<int>> thisRound = edges;
         while (true) {
-            vector<vector<int>> nextRound;
-            // int thisRound = 0;
-            for (int i = 0; i < thisRound.size(); i++) {
-                int from = thisRound[i][0], to = thisRound[i][1];
-                if (0 == counters[from] && 0 == counters[to]) {
-                    nextRound.push_back(thisRound[i]);
+            int thisRound = 0;
+            for (int i = 0; i < edges.size(); i++) {
+                int first = edges[i][0], second = edges[i][1];
+                if (first > second || 0 == counters[first] && 0 == counters[second]) {
                     continue;
                 }
 
-                if (1 == counters[from] && 1 == counters[to]) {
+                if (1 == counters[first] && 1 == counters[second]) {
                     return false;
                 }
 
-                if (0 == counters[from]) {
-                    counters[from] = 1;
+                if (0 == counters[first]) {
+                    counters[first] = 1;
                 } else {
-                    counters[to] = 1;
+                    counters[second] = 1;
                 }
 
-                // edges[i][1] = to;
+                edges[i][0] = second;
+                edges[i][1] = first;
                 resolved++;
-                // thisRound++;
+                thisRound++;
             }
 
-            if (0 == nextRound.size() || thisRound.size() == nextRound.size()) {
+            if (0 == thisRound) {
                 break;
             }
-
-            thisRound = nextRound;
         }
 
         return n == resolved;
