c58a90f337814329b27fe8141e0cc2450b543215
hackingworm <irvine.ye@outlook.com>
2025-03-20 12:02:39 +0800

    Time: 0 ms (100%), Space: 12.4 MB (67.73%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -0,0 +1,30 @@
+#define BRUTE 1
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        #if BRUTE
+        int i;
+        for (i = 0; i < nums.size(); i++) {
+            if ((0 == i || nums[i - 1] < nums[i])
+                && (nums.size() == i + 1 || nums[i] > nums[i + 1])) {
+                break;
+            }
+        }
+
+        return i;
+        #else
+        int left = 0, right = nums.size();
+        while (left < right) {
+            int middle = (left + right) / 2;
+            if (nums[middle] < nums[middle + 1]) {
+                left = middle + 1;
+            } else {
+                right = middle;
+            }
+        }
+
+        return right;
+        #endif        
+    }
+};
