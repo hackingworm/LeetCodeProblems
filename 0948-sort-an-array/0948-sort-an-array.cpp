ed6ce1acaa0d83b50a9b35e49b781fc113733f13
hackingworm <irvine.ye@outlook.com>
2025-03-26 15:37:33 +0800

    Time: 51 ms (71.95%), Space: 70.7 MB (98.76%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,6 +1,13 @@
+enum Algorithm {
+    MergeSort,
+    HeapSort,
+    QuickSort
+};
+
 class Solution {
+    Algorithm algo;
 public:
-    vector<int> merge(vector<int> nums1, vector<int> nums2) {
+    vector<int> merge(vector<int>& nums1, vector<int>& nums2) {
         vector<int> nums(nums1.size() + nums2.size());
         int i, j, k;
         for (i = 0, j = 0, k = 0; nums1.size() > i && nums2.size() > j;) {
@@ -28,10 +35,55 @@ public:
         }
 
         int mid = (begin + end) / 2;
-        return merge(mergeSort(nums, begin, mid), mergeSort(nums, mid, end));
+        vector<int> nums1 = mergeSort(nums, begin, mid);
+        vector<int> nums2 = mergeSort(nums, mid, end);
+        return merge(nums1, nums2);
+    }
+
+    void heapify(vector<int>& nums, int idx, int end) {
+        while (end / 2 > idx) {
+            int can = 2 * idx + 1;
+            if (end > can + 1 && nums[can] < nums[can + 1]) {
+                can++;
+            }
+
+            if (nums[idx] >= nums[can]) {
+                break;
+            }
+            
+            int tmp = nums[idx];
+            nums[idx] = nums[can];
+            nums[can] = tmp;
+            idx = can;
+        }
+    }
+
+    void buildHeap(vector<int>& nums) {
+        for (int i = (nums.size() - 1) / 2; i >= 0; i--) {
+            heapify(nums, i, nums.size());
+        }
+    }
+
+    vector<int> heapSort(vector<int>& nums) {
+        buildHeap(nums);
+        for (int i = nums.size() - 1; i > 0; i--) {
+            int tmp = nums[0];
+            nums[0] = nums[i];
+            nums[i] = tmp;
+            heapify(nums, 0, i);
+        }
+
+        return nums;
     }
 
     vector<int> sortArray(vector<int>& nums) {
-        return mergeSort(nums, 0, nums.size());
+        algo = HeapSort;
+        if (MergeSort == algo) {
+            return mergeSort(nums, 0, nums.size());
+        } else if (HeapSort == algo) {
+            return heapSort(nums);
+        } else if (QuickSort == algo) {}
+
+        return {};
     }
 };
