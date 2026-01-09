8ffd98a0a8a2dc73a402bd2f9c81dda6c67e4b2e
hackingworm <irvine.ye@outlook.com>
2026-01-09 18:12:06 +0800

    Time: 129 ms (31.79%), Space: 23.4 MB (51.5%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.py
+++ 0403-frog-jump/0403-frog-jump.py
@@ -22,23 +22,16 @@ class Solution:
 
             if self.stones[index + forward] < self.stones[index] + step + i:
                 forward += 1
-                #if len(self.stones) == index + forward:
-                #    return False
-                
                 continue
 
             if self.stones[index + forward] == self.stones[index] + step + i:
                 if self.canCrossWithStep(index + forward, step + i):
                     return True
 
-                #self.results[(index + forward, step + i)] = False
-
                 forward += 1
-                #if len(self.stones) == index + forward:
-                #    return False
 
             i += 1
 
-        #print(index, step)
         self.results[(index, step)] = False
+        
         return False
