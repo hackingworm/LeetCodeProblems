f9cf26abcd756136f53e2cc785ca5914021125e8
hackingworm <irvine.ye@outlook.com>
2026-01-19 22:36:11 +0800

    Time: 352 ms (13.88%), Space: 49.2 MB (33.76%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -1,31 +1,5 @@
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
-        #startTime, endTime, profit = self.sort(startTime, endTime, profit)
-
-        '''
-        for i in range(len(startTime)):
-            min = i
-            for j in range(i, len(startTime)):
-                if startTime[min] > startTime[j]:
-                    min = j
-
-            if i != min:
-                temp = startTime[i]
-                startTime[i] = startTime[min]
-                startTime[min] = temp
-                temp = endTime[i]
-                endTime[i] = endTime[min]
-                endTime[min] = temp
-                temp = profit[i]
-                profit[i] = profit[min]
-                profit[min] = temp
-
-        print(startTime)
-        print(endTime)
-        print(profit)
-        return 0
-        '''
-
         merged = [(0, 0, 0)] * len(startTime)
         for i in range(len(startTime)):
             merged[i] = (startTime[i], endTime[i], profit[i])
@@ -39,30 +13,6 @@ class Solution:
         maxProfit = self.jobSchedulingImpl(0)
         print("Hits", self.hit)
         return maxProfit
-
-    '''
-    def sort(self, startTime: List[int], endTime: List[int], profit: List[int]) -> Tuple[List[int], List[int], List:[int]]:
-        order = [0] * len(startTime)
-        for i in range(len(startTime)):
-            pos = 0
-            for j in range(len(startTime)):
-                if i != j and startTime[i] > startTime[j]:
-                    pos += 1
-            order[i] = pos
-
-        print(startTime)
-        print(order)
-
-        startTimeSorted = [0] * len(startTime)
-        endTimeSorted = [0] * len(endTime)
-        profitSorted = [0] * len(profit)
-        for i in range(len(startTime)):
-            startTimeSorted[order[i]] = startTime[i]
-            endTimeSorted[order[i]] = endTime[i]
-            profitSorted[order[i]] = profit[i]
-
-        return startTimeSorted, endTimeSorted, profitSorted
-    '''
 
     def jobSchedulingImpl(self, start: int) -> int:
         if start == len(self.profits):
@@ -80,8 +30,6 @@ class Solution:
         end = len(self.profits)
         while begin < end:
             middle = (begin + end) // 2
-            #if profits[0][1] == profits[middle][0]:
-            #    break
 
             if self.profits[start][1] <= self.profits[middle][0]:
                 end = middle
