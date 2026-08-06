5368dcff64b552b9861af498c14612679efb926c
hackingworm <irvine.ye@outlook.com>
2026-08-06 09:46:04 +0800

    Time: 3355 ms (12.53%), Space: 19.9 MB (99.24%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.py
@@ -0,0 +1,62 @@
+class Solution:
+    def adjacent(self, wordA: str, wordB: str) -> bool:
+        diffFound = False
+        for i in range(len(wordA)):
+            if wordA[i] != wordB[i]:
+                if diffFound:
+                    diffFound = False
+                    break
+                
+                diffFound = True
+
+        return diffFound
+
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        #if endWord != wordList[-1]:
+        #    return 0
+    
+        #if beginWord in wordList:
+        #    wordList.remove(beginWord)
+
+        currents = {beginWord}
+        level = 1
+
+        while currents and wordList:
+            nexts = set()
+            for current in currents:
+                for word in wordList:
+                    if self.adjacent(current, word):
+                        if endWord == word:
+                            return level + 1
+
+                        nexts.add(word)
+
+            #print(level, nexts, wordList)
+            for next in nexts:
+                wordList.remove(next)
+
+            #print(level, nexts, wordList)
+
+            currents = nexts
+            level += 1
+
+        return 0
+
+        '''
+        if beginWord == endWord:
+            return 1
+
+        min = 1 + len(wordList)
+        for i in range(len(wordList)):
+            if self.adjacent(beginWord, wordList[i]):
+                #print(wordList[i], endWord, wordList[:i] + wordList[i + 1:])
+                length = self.ladderLength(wordList[i], endWord, wordList[:i] + wordList[i + 1:])
+                #print(length)
+                if 0 < length and min > length:
+                    min = length
+
+        if 1 + len(wordList) == min:
+            return 0
+
+        return min + 1
+        '''
