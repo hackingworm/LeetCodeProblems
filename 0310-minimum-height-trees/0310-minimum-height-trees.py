5d53fb270e32e7d49c9735d8c561be56c4742142
hackingworm <irvine.ye@outlook.com>
2026-08-21 22:50:48 +0800

    Time: 3569 ms (5.01%), Space: 27.7 MB (89.8%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.py
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -1,10 +1,27 @@
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
-        if 1 == n:
-            return [0]
-        
-        if 2 == n:
-            return [0, 1]
+        '''
+        rootMaybe = 0
+        while True:
+            if 0 == len(edges):
+                return [rootMaybe]
+                
+            if 1 == len(edges):
+                return edges[0]
+
+            map = dict()
+            for edge in edges:
+                for i in range(2):
+                    if edge[i] in map:
+                        rootMaybe = edge[i]
+                        map[rootMaybe] = None
+                    else:
+                        map[edge[i]] = edge
+
+            for number, edge in map.items():
+                if edge:
+                    edges.remove(edge)
+        '''
 
         connection = dict()
         for edge in edges:
@@ -18,13 +35,22 @@ class Solution:
             else:
                 connection[edge[1]] = [edge[0]]
 
+        rootMaybe = 0
         while True:
+            if 0 == len(connection):
+                return [rootMaybe]
+
+            if 2 == len(connection):
+                roots = []
+                for number in connection:
+                    roots.append(number)
+                return roots
+    
             leaves = []
             for number, neighbors in connection.items():
                 if 1 == len(neighbors):
                     leaves.append(number)
 
-            rootMaybe = None
             for leaf in leaves:                
                 rootMaybe = connection[leaf][0]
                 connection[rootMaybe].remove(leaf)
@@ -32,13 +58,3 @@ class Solution:
                     del connection[rootMaybe]
            
                 del connection[leaf]
-
-            if 2 == len(connection):
-                roots = []
-                for number in connection:
-                    roots.append(number)
-
-                return roots
-
-            if 0 == len(connection):
-                return [rootMaybe] 
