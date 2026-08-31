326281c205c67f4a48ae48fbc4ff45a947fad6ac
hackingworm <irvine.ye@outlook.com>
2026-09-01 03:06:39 +0800

    Time: 2 ms (47.22%), Space: 19.3 MB (86.32%) - LeetHub

---
+++ 0889-construct-binary-tree-from-preorder-and-postorder-traversal/0889-construct-binary-tree-from-preorder-and-postorder-traversal.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def constructFromPrePost(self, preorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        if len(preorder) == 0:
+            return None
+
+        current = root = TreeNode(preorder[0])
+        i = 1
+        while i < len(preorder) and preorder[i] == postorder[len(postorder) - i - 1]:
+            current.left = TreeNode(preorder[i])
+            current = current.left
+            i += 1
+        #print("i ", i)
+
+        if len(preorder) == i:
+            return root
+
+        j = 0
+        while preorder[i] != postorder[j]:
+            j += 1  
+        #print("j ", j)
+        #print(preorder[i: i + j + 1], preorder[i + j + 1:], postorder[:j + 1], postorder[j + 1: len(postorder) - i])
+        current.left = self.constructFromPrePost(preorder[i: i + j + 1], postorder[:j + 1])
+        current.right = self.constructFromPrePost(preorder[i + j + 1:], postorder[j + 1: len(postorder) - i])
+
+        return root
